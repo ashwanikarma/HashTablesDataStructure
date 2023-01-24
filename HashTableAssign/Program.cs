554f2269b60a7ad1bfc596f1e81fd932328f9413
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-24 22:53:00 +0530

    [Ashwani] - Added hash map for frequency of words in paragraph

--- HashTableAssign/Program.cs
+++ HashTableAssign/Program.cs
@@ -5,48 +5,53 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table Practice Problems!!!");
-                                     
-                    string frequency = "To be or not to be";
-                    List<string> compareList = new List<string>();
-                    List<string> storeCountList = new List<string>();
-                    string[] frequencyArray = frequency.Split(' ');
-                    MyMapNode<int, string> hash1 = new MyMapNode<int, string>(frequencyArray.Length);
-                    for (int i = 0; i < frequencyArray.Length; i++)
-                    {
-                        hash1.Add(i, frequencyArray[i]);
 
-                        Console.Write(hash1.Get(i) + " ");
-                    }
-                    Console.WriteLine();
-                    for (int i = 0; i < frequencyArray.Length; i++)
-                    {
-                        int count = 0;
-                        for (int j = 0; j < frequencyArray.Length; j++)
-                        {
-                            if (frequencyArray[j].ToLower() == hash1.Get(i).ToLower())
-                            {
-                                count++;
+            string frequency2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-                                if (compareList.Contains(hash1.Get(j).ToLower()))
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                        if (compareList.Contains(hash1.Get(i).ToLower()))
-                        {
-                            continue;
-                        }
-                        compareList.Add(hash1.Get(i));
+            List<string> compareList2 = new List<string>();
+            List<string> storeCountList2 = new List<string>();
 
-                        storeCountList.Add(hash1.Get(i) + "\t" + count);
-                    }
+            string[] frequencyArray2 = frequency2.Split(' ');
 
-                    Console.WriteLine("Word and their frequencies shown below");
-                    for (int i = 0; i < storeCountList.Count; i++)
-                    {
-                        Console.WriteLine(storeCountList[i]);
-                    }
+            MyMapNode<int, string> hash2 = new MyMapNode<int, string>(frequencyArray2.Length);
+
+            Console.WriteLine("Given phrase is :\n");
+            for (int i = 0; i < frequencyArray2.Length; i++)
+            {
+                hash2.Add(i, frequencyArray2[i]);
+
+                Console.Write(hash2.Get(i) + " ");
             }
+            Console.WriteLine("\n");
+            for (int i = 0; i < frequencyArray2.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < frequencyArray2.Length; j++)
+                {
+                    if (frequencyArray2[j].ToLower() == hash2.Get(i).ToLower())
+                    {
+                        
+                        count++;
+
+                        if (compareList2.Contains(hash2.Get(j).ToLower()))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (compareList2.Contains(hash2.Get(i).ToLower()))
+                {
+                    continue;
+                }
+                compareList2.Add(hash2.Get(i));
+
+                storeCountList2.Add(hash2.Get(i) + "\t" + count);
+            }
+            Console.WriteLine("Word and their frequencies shown below");
+            for (int i = 0; i < storeCountList2.Count; i++)
+            {
+                Console.WriteLine(storeCountList2[i]);
+            }
+        }
         }
     }
